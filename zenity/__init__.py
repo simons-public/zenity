b5b65ec34bd3509f654405f78756f32e69955606
Chris Simons <chris+git@simonsmail.net>
2018-11-25 11:24:35 -0800

    cleanup

--- zenity/__init__.py
+++ zenity/__init__.py
@@ -1,6 +1,5 @@
 """ Simpoe python API for zenity """
 
-#from .zenity import Info, Warn, Progress, Input, File
 from .zenity import (Info, Warn, Question, Error, Progress,
                      Input, FileSelect, Notification, Scale,
                      Calendar, List, Color, Password, Text,
